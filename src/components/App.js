8c160a64a85e0a645090a38817654ac49abdfe5a
justinkim2015 <justinkim2015@gmail.com>
2022-12-28 17:27:13 +0900

    Add basic styling to home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
+import logo from '../assets/stufflogo.png'
+import '../styles/app.css'
+import { Link } from 'react-router-dom'
+
 function Home() {
   return (
-    <div>
-      Hello World!
+    <div id='homepage'>
+      <h1 id='greeting'>We sell stuff.</h1>
+      <Link to='/shop' class="nav-link"><img src={logo}></img></Link>
+      <p id='subgreeting'>Come check us out!</p>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import '../styles/navbar.css'
 const Navbar = () => {
   return (
     <div id="navbar">
-      <h1 id='nav-title'>Stuff for Sale!</h1>
+      <h1 id='nav-title'>Stuff!</h1>
       <Link to='/' class="nav-link">Home</Link>
       <Link to='/shop' class="nav-link">Shop</Link>
     </div>
