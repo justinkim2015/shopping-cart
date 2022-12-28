0d13eee73072fe5812e9c483d661c8bd67e0ed5e
justinkim2015 <justinkim2015@gmail.com>
2022-12-28 17:16:58 +0900

    Add basic styling to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,9 @@ import '../styles/navbar.css'
 const Navbar = () => {
   return (
     <div id="navbar">
-      <Link to='/'>Home</Link>
-      <Link to='/shop'>Shop</Link>
+      <h1 id='nav-title'>Stuff for Sale!</h1>
+      <Link to='/' class="nav-link">Home</Link>
+      <Link to='/shop' class="nav-link">Shop</Link>
     </div>
   )
 }
